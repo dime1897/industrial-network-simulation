a11d88f43a5affaa87f072e6979ca1f74e1c68d8
dime1897 <diego.mercoliano@live.com>
2024-08-27 17:26:14 +0200

    new filsystem

---
+++ code/src/devices/PLCsiemens/AreaTypes.py
@@ -0,0 +1,11 @@
+class My_Int:
+    value: int
+    def __init__(self, num=int):
+        self.value=num
+
+class Areas:
+
+    PA = My_Int(1)
+    PE = My_Int(0)
+    MK = My_Int(2)
+    DB = My_Int(5)
