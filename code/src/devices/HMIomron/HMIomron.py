ea39ae5649547f980721f8c62a56c273b620f5c2
dime1897 <diego.mercoliano@live.com>
2024-09-05 17:55:28 +0200

    Fins comunicante

--- code/src/devices/HMIomron/HMIomron.py
+++ code/src/devices/HMIomron/HMIomron.py
@@ -1,11 +1,11 @@
 import time
-from fins import FinsClient
+from fins.client import FinsClient
 
-client = FinsClient(host="192.168.172.151", port=9600)
+client = FinsClient(host="172.19.3.1", port=9600)
 client.connect()
 
 dato = 634
-client.memory_area_write('D1850', dato.to_bytes(8, 'big'), 1)
+client.memory_area_write('D1850', dato.to_bytes(2, 'big'), 1)
 
 for i in range(100):
     addr = 'D' + str(1800+i)
