414d5624e47b99d072529aac77d008a24f47ee6e
dime1897 <diego.mercoliano@live.com>
2024-09-06 12:37:07 +0200

    update

--- code/src/devices/HMIomron/HMIomron.py
+++ code/src/devices/HMIomron/HMIomron.py
@@ -1,10 +1,9 @@
-import time
 from fins.client import FinsClient
 
-client = FinsClient(host="172.19.3.1", port=9600)
+client = FinsClient(host="172.20.3.1", port=9600)
 client.connect()
 
-dato = 634
+dato = 12456
 client.memory_area_write('D1850', dato.to_bytes(2, 'big'), 1)
 
 for i in range(100):
